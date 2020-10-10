3e2606edd1141087afc29b4770f5d75709af42f3
Frank Wei <frank@frankwei.xyz>
2020-10-10 16:57:32 +0800

    fix: typo MinerManager

--- src/Util/MinerManager.sol
+++ src/Util/MinerManager.sol
@@ -5,19 +5,19 @@ pragma solidity ^0.6.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MinerManager is Ownable {
-    mapping (address => bool) public minters;
+    mapping (address => bool) public miners;
 
     constructor() internal Ownable() {}
 
     function addMiner(address _minter) public onlyOwner {
-        minters[_minter] = true;
+        miners[_minter] = true;
     }
     function removeMiner(address _minter) public onlyOwner {
-        minters[_minter] = false;
+        miners[_minter] = false;
     }
 
     modifier onlyMiner() {
-        require(minters[msg.sender], "Error: only miner");
+        require(miners[msg.sender], "Error: only miner");
         _;
     }
 }
